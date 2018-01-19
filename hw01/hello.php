1e8927940083d2d6638346324a4571cf8601de6a
Miller R Atkinson <MRAtkinson@hsutx.edu>
2018-01-19 11:20:22 -0600

    first commit, Lab Day 1

---
+++ hw01/hello.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Hello, World!</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+	</head>
+
+	<body>
+		<?php
+			echo date("F j, Y, g:i a");
+		?>
+	</body>
+</html>
